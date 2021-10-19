b2c7b965dea6d098d260ea0d88d7bbc34b0d5822
leparwa <kevin.leparwa@student.moringaschool.com>
2021-10-19 11:23:09 +0300

    merge and update internvetion

--- ireporterApp/urls.py
+++ ireporterApp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('redflag', RedFlagList.as_view(), name='red-flag-list'),
     path('redflag/<int:pk>', RedFlagDetail.as_view(), name='red-flag-details'),
     path('admin/redflag/<int:pk>', RedFlagAdminActions.as_view(), name='red-flag-details-admin'),
-    path('interventionlist', InterventionList.as_view(), name='intervention-list'),
+    path('intervention', InterventionList.as_view(), name='intervention-list'),
     path('intervention/<int:pk>',InterventionDetail.as_view(), name='intervention-details'),
 ]
 
